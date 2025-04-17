586aa401e5e871e0f538e4b3c93f95e7e86af7cb
하진 권씨 <hajin_2056@naver.com>
2025-04-17 22:03:34 +0900

    CREATE :: Backend 프로젝트 생성

---
+++ saejungang_place_reservation/src/main/java/saejungang/saejungang_place_reservation/SaejungangPlaceReservationApplication.java
@@ -0,0 +1,13 @@
+package saejungang.saejungang_place_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaejungangPlaceReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaejungangPlaceReservationApplication.class, args);
+    }
+
+}
