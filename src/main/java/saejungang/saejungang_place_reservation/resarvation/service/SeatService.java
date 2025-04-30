091ef2039eee1b75fe6f140200585a7ac071050b
하진 권씨 <hajin_2056@naver.com>
2025-04-30 15:46:58 +0900

    CREATE :: SeatService/impl 만들어야할 메소드 정리

--- src/main/java/saejungang/saejungang_place_reservation/resarvation/service/SeatService.java
+++ src/main/java/saejungang/saejungang_place_reservation/resarvation/service/SeatService.java
@@ -1,7 +1,18 @@
 package saejungang.saejungang_place_reservation.resarvation.service;
 
 import org.springframework.http.ResponseEntity;
+import saejungang.saejungang_place_reservation.resarvation.entity.SeatEntity;
+
+import java.util.List;
 
 public interface SeatService {
     ResponseEntity<Object> reservation();
+
+    ResponseEntity<Object> getAllSeats();
+
+    ResponseEntity<Object> getSeatById(Long Id);
+
+    ResponseEntity<Object> reserveSeat(List<SeatEntity> seatEntityList);
+
+    ResponseEntity<Object> cancelReserveSeat(Long SeatId);
 }
--- src/main/java/saejungang/saejungang_place_reservation/resarvation/service/impl/SeatServiceImpl.java
+++ src/main/java/saejungang/saejungang_place_reservation/resarvation/service/impl/SeatServiceImpl.java
@@ -1,13 +1,51 @@
 package saejungang.saejungang_place_reservation.resarvation.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import saejungang.saejungang_place_reservation.resarvation.entity.SeatEntity;
+import saejungang.saejungang_place_reservation.resarvation.repository.SeatRepository;
 import saejungang.saejungang_place_reservation.resarvation.service.SeatService;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class SeatServiceImpl implements SeatService {
+
+    private final SeatRepository seatRepository;
+    Map<String, Object> returnData = new HashMap<>();
+
+    @Autowired // 생성자 주입
+    public SeatServiceImpl(SeatRepository seatRepository) {
+        this.seatRepository = seatRepository;
+    }
     @Override
     public ResponseEntity<Object> reservation() {
         return ResponseEntity.ok("Hello World");
+    }
+
+    @Override
+    public ResponseEntity<Object> getAllSeats() {
+        returnData.put("seats", seatRepository.findAllSeats());
+        return ResponseEntity.ok(returnData);
+    }
+
+    @Override
+    public ResponseEntity<Object> getSeatById(Long Id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> reserveSeat(List<SeatEntity> seatEntityList) {
+
+
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> cancelReserveSeat(Long SeatId) {
+        return null;
     }
 }
