6af1b3b02b4fd1cddd992738962d82aed5ac1bac
하진 권씨 <hajin_2056@naver.com>
2025-05-01 21:52:21 +0900

    CREATE :: reservation api

--- src/main/java/saejungang/saejungang_place_reservation/resarvation/controller/SeatController.java
+++ src/main/java/saejungang/saejungang_place_reservation/resarvation/controller/SeatController.java
@@ -2,9 +2,13 @@ package saejungang.saejungang_place_reservation.resarvation.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import saejungang.saejungang_place_reservation.resarvation.entity.SeatEntity;
 import saejungang.saejungang_place_reservation.resarvation.service.SeatService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/place")
@@ -14,5 +18,10 @@ public class SeatController {
     @PostMapping("/reserve")
     public ResponseEntity<Object> reservation() {
         return placeService.reservation();
+    }
+
+    @PutMapping("/reservation")
+    public ResponseEntity<Object> reserve_seats(List<SeatEntity> seatEntityList) {
+        return ResponseEntity.ok(placeService.reserveSeat(seatEntityList));
     }
 }
